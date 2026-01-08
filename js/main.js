f24ccc88c917b7a1ad05eae9386284159925f066
JoelContreras-Code <joel.contreras@holdingvml.net>
2026-01-08 16:15:51 -0500

    Se agrega el .gitignore y se realiza un js basico a un boton

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log("JavaScript conectado correctamente.");
+
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", () => {
+    alert("Login próximamente");
+});
